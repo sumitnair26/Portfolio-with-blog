ef9958153c400437daefdef337c229f03839cecd
sumitnair26 <sumit.nair26@gmail.com>
2023-06-25 15:26:08 +0530

    Added Footer and social media Icons

---
+++ components/SiteFooter.vue
@@ -0,0 +1,22 @@
+<script setup>
+const links = [
+  { name: 'GitHub', href: 'https://github.com/sumitnair26', icon: 'entypo-social:github-with-circle' },
+  { name: 'Twitter', href: 'https://twitter.com/sumitnair26', icon: 'entypo-social:twitter' },
+  { name: 'Facebook', href: 'https://facebook.com/sumitnair26', icon: 'entypo-social:facebook' },
+  { name: 'LinkedIn', href: 'https://www.linkedin.com/in/sumitnair26/', icon: 'entypo-social:linkedin-with-circle' },
+  { name: 'Instagram', href: 'https://instagram.com/sumitnair26', icon: 'entypo-social:instagram' }
+]
+</script>
+
+<template>
+  <footer class="bg-gray-800">
+    <section class="mx-auto max-w-7xl px-2 sm:px-4 lg:py-6 text-gray-300 flex flex-row">
+      <p class="flex-1">&copy; Sumit Nair 2023. All Rights Reserved</p>
+      <div class="flex flex-row justify-center gap-4 mb-4 mt-2">
+        <a v-for="link in links" :key="link.name" :href="link.href">
+          <Icon :name="link.icon" size="1.5rem" class="text-gray-300" />
+        </a>
+      </div>
+    </section>
+  </footer>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,8 @@
 
 <template>
   <section>
-    <h1 class="text-5xl font-bold mt-20">👋🏻 Hello, I'm Dan Vega!</h1>
-    <p class="text-base text-gray-900 p-2 italic">Spring Developer Advocate at VMware</p>
+    <h1 class="text-5xl font-bold mt-20">👋🏻 Hello, I'm Sumit Nair!</h1>
+    <p class="text-base text-gray-900 p-2 italic">Full Stack Developer at I-Exceed</p>
   </section>
   <section class="flex flex-col md:flex-row">
     <div class="md:w-3/4">
@@ -20,7 +20,7 @@
         I have been writing software for over 9+ years now.
       </p>
       <p class="text-lg py-2">
-        In my personal life I lke Bike Riding, listing to music, Exploring new Places and Spend time with Family and Frieds .
+        In my personal life I lke Bike Riding, listing to music, Exploring new Places and Spend time with Family and Friends .
       </p>
     </div>
     <img src="~/assets/images/sumit-nair.jpg" class="w-1/2 md:max-w-sm p-8 mx-auto" />
