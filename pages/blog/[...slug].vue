d84c3e46a1961d210c8e4a8ba4dbc8541b71e2bf
sumitnair26 <sumit.nair26@gmail.com>
2023-06-26 20:38:39 +0530

    Adding Pages to blog

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,6 +1,28 @@
+<script setup>
+    const {path} = useRoute();
+
+    const { data } = await useAsyncData(`contant-${path}`, () => {
+        return queryContent()
+            .where({ _path: path})
+            .findOne()
+    })
+
+</script>
+
+<!-- 
 <template>
-<!-- <p>{{ $route.params.slug }}</p> -->
-  <main>
-    <ContentDoc class="prose" />
-  </main>
+<p>{{ $route.params.slug }}</p> 
+  
+    <ContentRenderer :value="data" class="prose my-10 mx-auto mx-auto max-w-7xl"  />
+
+</template>
+-->
+<template>
+  <ContentRenderer :value="data" class="prose my-10 mx-auto max-w-7xl" />
+  <div class="my-8">
+    <a v-for="tag in  data.tags " :key="tag" :href="`/blog/tags/${tag}`"
+      class="text-sm font-semibold inline-block py-2 px-4 rounded-lg text-gray-100 bg-blue-500 uppercase last:mr-0 mr-4">
+      <Icon name="pajamas:label" size="1.5rem" class="text-gray-100 mr-2" /> {{ tag }}
+    </a>
+  </div>
 </template>
